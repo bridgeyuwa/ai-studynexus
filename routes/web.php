cd0e0ee72a40033e0e6a1a9d40b71a5c8513708f
bridgeyuwa <bridgeyuwa@gmail.com>
2025-06-21 02:09:13 +0100

    removed prototype routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AIController;
 use App\Livewire\Search;
-
-
-Route::get('/dfghj', [AIController::class, 'index'])->name('home');
-
-Route::post('/ask', [AIController::class, 'answer'])->name('ask');
 
 Route::get('/', Search::class);
